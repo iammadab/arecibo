14c2af6199c32dd6d81094252d835e1fe60e7a4e
Wisdom Ogwu <iammadab@gmail.com>
2024-04-22 10:49:27 +0100

    update documentation for lookuptrace builder

--- src/gadgets/lookup/lookup_trace_builder.rs
+++ src/gadgets/lookup/lookup_trace_builder.rs
@@ -1,11 +1,3 @@
-// this is supposed to build and return the LookupTrace
-// operations:
-//  new
-//  read
-//  write
-//  snapshot
-//  get_challenges (should this be here?)
-
 use crate::constants::NUM_CHALLENGE_BITS;
 use crate::gadgets::lookup::lookup_table::LookupTable;
 use crate::gadgets::lookup::lookup_trace::{LookupTrace, RWTrace};
@@ -14,33 +6,26 @@ use ff::PrimeField;
 use std::collections::BTreeMap;
 use std::mem;
 
-// TODO: add documentation
+/// Performs read write operations on a `LookupTable` while keeping track
+/// of each operation in a trace vector
 struct LookupTraceBuilder<'a, E: CurveCycleEquipped> {
-  // needs a lookup table to get the actual values from and also to write them to
-  // why does this need a cache??? thinking transaction related stuff so will just go with it
-  // and a trace
-  // TODO: add documentation for each piece
   lookup_table: &'a mut LookupTable<E::Scalar>,
-  // TODO: this could be the lookup trace itself
   trace: Vec<RWTrace<E::Scalar>>,
-  lookup_table_cache: BTreeMap<E::Scalar, (E::Scalar, E::Scalar)>,
 }
 
 impl<'a, E: CurveCycleEquipped> LookupTraceBuilder<'a, E>
 where
   E::Scalar: Ord,
 {
-  // TODO: add documentation
+  /// Instantiate a new `LookupTraceBuilder` for a `LookupTable`
   fn new(lookup_table: &'a mut LookupTable<E::Scalar>) -> Self {
-    // TODO: does this need to do any extra checks?
     Self {
       lookup_table,
       trace: vec![],
-      lookup_table_cache: BTreeMap::new(),
     }
   }
 
-  // TODO: add documentation
+  /// Performs and records a read operation on the `LookupTable`
   fn read(&mut self, addr: E::Scalar) -> E::Scalar {
     let (read_value, read_ts, _, write_ts) = self.lookup_table.rw_operation(addr, None);
     self
@@ -49,7 +34,7 @@ where
     read_value
   }
 
-  // TODO: add documentation
+  /// Performs and records a write operation on the `LookupTable`
   fn write(&mut self, addr: E::Scalar, value: E::Scalar) {
     let (read_value, read_ts, write_value, write_ts) =
       self.lookup_table.rw_operation(addr, Some(value));
@@ -62,20 +47,13 @@ where
     ));
   }
 
-  // TODO: add documentation
+  /// Generates the next intermediate gamma and returns the `LookupTrace`
   fn snapshot(
     &mut self,
     ro_consts: ROConstants<Dual<E>>,
     prev_intermediate_gamma: E::Scalar,
   ) -> (E::Scalar, LookupTrace<E>) {
-    // the goal here is to update the intermediate gamma with every element in the trace
-    // technically we could have done this during read / write
-    // for this we need a hasher, the hasher seems to use values from the base field
-    let mut hasher = <Dual<E> as Engine>::RO::new(
-      ro_consts,
-      // TODO: fix the number of absorbs, intentionally making it wrong to see how this affects things
-      1,
-    );
+    let mut hasher = <Dual<E> as Engine>::RO::new(ro_consts, 1 + 5 * self.trace.len());
 
     hasher.absorb(prev_intermediate_gamma);
 
@@ -97,10 +75,12 @@ where
       hasher.absorb(*write_ts);
     }
 
-    // then squeeze out the next intermediate gamma
+    // squeeze out the next intermediate gamma
     let next_intermediate_gamma = scalar_as_base::<Dual<E>>(hasher.squeeze(NUM_CHALLENGE_BITS));
     let trace = mem::take(&mut self.trace);
 
     (next_intermediate_gamma, LookupTrace::new(trace))
   }
+
+  // TODO: implement get_challenges
 }
